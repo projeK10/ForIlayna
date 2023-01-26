c51fb6994f71b3b8128c5de21f6bf296ed0d59eb
projeK10 <101906430+projeK10@users.noreply.github.com>
2023-01-26 18:39:29 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const mainButton = document.getElementById("eventSwitch");
+const textChange = document.getElementById("changeText");
+const loveWords = [
+  "adore",
+  "cherish",
+  "treasure",
+  "appreciate",
+  "care for",
+  "admire",
+  "respect",
+  "have affection for",
+  "am devoted to",
+  "craze over",
+  "am charmed by",
+  "am joyful because of",
+  "am elated because of",
+  "am estatic because of",
+  "am obsessed with",
+  "care deeply for",
+  "care a lot for",
+];
+let phrase;
+let lastString;
+let randomNumber;
+let continueThis;
+mainButton.addEventListener("click", changeWord);
+
+function changeWord() {
+  lastString = randomNumber;
+  randomNumber = getRandomNumber();
+  continueThis = checkThis();
+  if (continueThis) {
+    phrase = loveWords[randomNumber];
+    textChange.textContent = `I ${phrase} Ilayna`;
+  } else {
+    runThisAgain();
+  }
+}
+
+function runThisAgain() {
+  changeWord();
+}
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 17);
+}
+
+function checkThis() {
+  if (lastString === randomNumber) {
+    return false;
+  } else {
+    return true;
+  }
+}
